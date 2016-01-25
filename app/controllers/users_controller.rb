56d80b7b43ae213f31d0be4968347bfbe3833383
App Academy Student <student@appacademy.io>
2016-01-25 14:19:08 -0800

    Add show route and render all as json in #index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    user = User.all
+    render json: user.to_json
+  end
+
+  def show
+    render json: { 'a_key' => 'a_value' }
+  end
 end
