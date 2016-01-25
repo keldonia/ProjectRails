8e0efca4eb1436cd3b3dceeb7bcfb788193b2b74
App Academy Student <student@appacademy.io>
2016-01-25 14:55:34 -0800

    Add #create, #update, #destroy, & #params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,37 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: { 'a_key' => 'a_value' }
+    user = User.find(params[:id])
+    render json: user.to_json
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render(
+          json: user.errors.full_messages, status: :unprocessable_entity
+        )
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user.to_json
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy!
+    # render json: user.to_json
+    redirect_to users_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
