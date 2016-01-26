47db623592baee2568638b871e37c883d5ebd554
App Academy Student <student@appacademy.io>
2016-01-25 17:22:17 -0800

    Nest contact#index into user, add sharedcontacts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create, :destroy, :index, :show, :update] do
+    resources :contacts, only: [:index]
+  end
+  resources :contacts, only: [:create, :destroy, :show, :update]
+  resources :contact_shares, only: [:create, :destroy]
   # get 'users/' => 'users#index'
   # post 'users/' => 'users#create'
   # get 'users/new' => 'users#new', :as => 'new_user'
