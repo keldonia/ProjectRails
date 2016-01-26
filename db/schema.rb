d799e5c8bd647e6ce73aeae7f141c01438bc87e2
App Academy Student <student@appacademy.io>
2016-01-25 17:18:53 -0800

    Create contact_shares table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125230818) do
+ActiveRecord::Schema.define(version: 20160125232345) do
+
+  create_table "contact_shares", force: :cascade do |t|
+    t.integer  "contact_id", null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "contact_shares", ["contact_id", "user_id"], name: "index_contact_shares_on_contact_id_and_user_id", unique: true
+  add_index "contact_shares", ["contact_id"], name: "index_contact_shares_on_contact_id"
+  add_index "contact_shares", ["user_id"], name: "index_contact_shares_on_user_id"
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name",       null: false
